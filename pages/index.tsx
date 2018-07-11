2ed4c366225b1c9d0e878063b2d27b1c2da5e1f3
Scott Jones <scottdj92@gmail.com>
2018-07-10 20:07:41 -0400

    god's plan

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+    color: cornflowerblue;
+`;
+
+const Index: React.SFC = () => (
+    <Title>Hello world</Title>
+);
+
+export default Index;
